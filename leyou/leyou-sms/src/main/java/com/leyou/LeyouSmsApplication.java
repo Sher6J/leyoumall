0181ef025aa246d11ce4ed6c0f79999e0633d3a0
Sher6J <liujx17614417627@outlook.com>
2020-04-29 22:34:04 +0800

    update

---
+++ leyou/leyou-sms/src/main/java/com/leyou/LeyouSmsApplication.java
@@ -0,0 +1,15 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sher6j
+ * @create 2020-04-29-22:13
+ */
+@SpringBootApplication
+public class LeyouSmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouSmsApplication.class);
+    }
+}
---
+++ leyou/leyou-sms/src/main/java/com/leyou/sms/listner/SmsListner.java
@@ -0,0 +1,46 @@
+package com.leyou.sms.listner;
+
+import com.aliyuncs.exceptions.ClientException;
+import com.leyou.sms.config.SmsProperties;
+import com.leyou.sms.utils.SmsUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Map;
+
+/**
+ * @author sher6j
+ * @create 2020-04-29-22:19
+ */
+@Component
+public class SmsListner {
+
+    @Autowired
+    private SmsUtils smsUtils;
+
+    @Autowired
+    private SmsProperties smsProperties;
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "LEYOU.SMS.QUEUE", durable = "true"),
+            exchange = @Exchange(value = "LEYOU.SMS.EXCHANGE", ignoreDeclarationExceptions = "true", type = ExchangeTypes.TOPIC),
+            key = {"verifycode_sms"}
+    ))
+    public void sendSms(Map<String, String> msg) throws ClientException {
+        if (CollectionUtils.isEmpty(msg)) {
+            return;
+        }
+        String phone = msg.get("phone"); //手机号
+        String code = msg.get("code"); //验证码
+        if (StringUtils.isNoneBlank(phone) && StringUtils.isNoneBlank(code)) {
+            this.smsUtils.sendSms(phone, code, this.smsProperties.getSignName(), this.smsProperties.getVerifyCodeTemplate());
+        }
+    }
+}
