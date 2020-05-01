b4bcdae518dae90b13d1f0e83ddd23b7b4053833
Sher6J <liujx17614417627@outlook.com>
2020-05-01 21:06:58 +0800

    update

--- leyou/leyou-auth/leyou-auth-service/src/main/java/com/leyou/auth/controller/AuthController.java
+++ leyou/leyou-auth/leyou-auth-service/src/main/java/com/leyou/auth/controller/AuthController.java
@@ -2,13 +2,17 @@ package com.leyou.auth.controller;
 
 import com.leyou.auth.config.JwtProperties;
 import com.leyou.auth.service.AuthService;
+import com.leyou.common.pojo.UserInfo;
 import com.leyou.common.utils.CookieUtils;
+import com.leyou.common.utils.JwtUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -45,5 +49,29 @@ public class AuthController {
         CookieUtils.setCookie(request, response, this.jwtProperties.getCookieName(), token, this.jwtProperties.getExpire() * 60);
 
         return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("verify")
+    public ResponseEntity<UserInfo> verify(
+            @CookieValue("LY_TOKEN") String token,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        try {
+            //通过jwt工具类使用公钥解析jwt
+            UserInfo user = JwtUtils.getInfoFromToken(token, this.jwtProperties.getPublicKey());
+
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+
+            //刷新jwt中有效时间
+            token = JwtUtils.generateToken(user, this.jwtProperties.getPrivateKey(), this.jwtProperties.getExpire());
+            //刷新cookie中的有效时间
+            CookieUtils.setCookie(request, response, this.jwtProperties.getCookieName(), token, this.jwtProperties.getExpire() * 60);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
